9ae63543322018856319c9f8279eefdfd2d888c5
xiaomage <1154699897@qq.com>
2023-07-05 14:21:14 +0800

    update

--- src/Common.php
+++ src/Common.php
@@ -25,25 +25,37 @@ if (!function_exists('domain')) {
 
 if (!function_exists('slog')) {
     /**
+     *
+     * 日常开发中记录的日志，默认是保存在laravel中，此记录是保存到debug.txt中
+     *
+     * 需要配置在config/logging.php文件中的channels中配置一下选项
+     *
+     *'debug' => [
+     *  'driver' => 'single',
+     *  'path' => storage_path('logs/debug.log'),
+     *  'level' => 'debug',
+     * ],
+     *
      * @param $message
      * @param string|array $data
      * @return bool
      */
-    function slog($message, $data = ''): bool
+    function slog($message, $data = null): bool
     {
-        if ($message && empty($data)) {
-            $data = [$message];
-            $message = 'debug';
-        } elseif ($message && $data) {
-            if (!is_array($data)) {
-                $data = [$data];
-            }
-        } else {
-            $message = 'debug';
-            $data = [];
+        if (is_array($message)) {
+            $data = $message;
+            $message = '';
         }
 
-        \Illuminate\Support\Facades\Log::debug($message, $data);
+        if ($data && !is_array($data)) {
+            $data = [$data];
+        }
+
+        if (config('logging.channels.debug')) {
+            \Illuminate\Support\Facades\Log::channel('debug')->debug($message, $data ?: []);
+        } else {
+            \Illuminate\Support\Facades\Log::debug($message, $data);
+        }
 
         return true;
     }
